dd9a082aa25f7ff4a373138e287265bb7cac5583
ascottmccauley <scott@ascottmccauley.com>
2015-04-09 15:05:55 -0400

    Add `#` and `role` to `#header`

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <?php get_template_part( 'head' ); ?>
 <?php // Homepage or First Visit
 if ( is_front_page() || groundup_is_new_user() ) { ?>
-	<header>
+	<header id="header" role="banner">
 		<h1 class="brand"><a href="<?php echo home_url(); ?>/"><?php bloginfo( 'name' ); ?></a></h1>
 		<?php if(get_bloginfo( 'description' ) != '') { ?>
 			<h2 class="description"><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'about' ) ) ); ?>"><?php echo get_bloginfo( 'description' ); ?></a></h2>
