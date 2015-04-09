0656a1ae799810753f6614812cdd799b12ca77cf
ascottmccauley <scott@ascottmccauley.com>
2015-04-09 12:52:38 -0400

    Add Mobile Menu to Nav

--- templates/nav.php
+++ templates/nav.php
@@ -4,7 +4,9 @@
  * @package groundup
  */
 ?>
-<?php if ( has_nav_menu( 'primary' ) ) { ?>
-	<nav><?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => 'false' ) ); ?></nav>
-<?php }
-
+<?php // Desktop Menu
+if ( wp_is_mobile() && has_nav_menu( 'mobile' ) ) {
+	wp_nav_menu( array( 'theme_location' => 'mobile', 'container' => 'nav' ) );
+} else if ( has_nav_menu( 'primary' ) ) {
+	wp_nav_menu( array( 'theme_location' => 'primary', 'container' => 'nav' ) );
+} 
