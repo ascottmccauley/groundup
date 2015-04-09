379db8aa7f91a93a1245ffe63c1943edc0bd3b77
ascottmccauley <scott@ascottmccauley.com>
2015-04-09 15:04:14 -0400

    Add footer menu to #footer

--- footer.php
+++ footer.php
@@ -7,12 +7,15 @@
 // html is opened in header.php
 // body is opened in header.php
 ?>
-<footer>
-	<div class="copyrights">
-		<a href="<?php echo esc_url(get_permalink(get_page_by_title( 'Copyrights'))); ?>">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></a>
-	</div>
+<footer id="footer">
+	<?php if ( has_nav_menu( 'mobile' ) ) {
+		wp_nav_menu( array( 'theme_location' => 'footer', 'container' => 'false' ) );
+	} else { ?>
+		<div class="copyrights">
+			<a href="<?php echo esc_url(get_permalink(get_page_by_title( 'Copyrights'))); ?>">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></a>
+		</div>
+	<?php } ?>
 </footer>
 <?php wp_footer(); ?>
-
 </body>
 </html>
