a726a65e576804d6b50367a6928281a2b47fec8a
ascottmccauley <scott@ascottmccauley.com>
2015-04-12 22:43:02 -0400

    Add pagination

---
+++ templates/pagination.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Determine if the page should be paginated, and add the correct type of pagination
+ *
+ * @package groundup
+ */
+?>
+<?php if ( is_single() ) {
+	$prev = ( is_attachment() ) ? get_post( $post->post_parent ) : get_adjacent_post( false, '', true );
+	$next = get_adjacent_post( false, '', false );
+	if ( $prev || $next ) { ?>
+		<nav id="pagination" class="row" role="navigation">
+			<?php if ( $next ) {
+				next_post_link('%link', '&laquo; Newer', true);
+			}
+			if ( $prev ) {
+				previous_post_link('%link','Older &raquo;', true);
+			} ?>
+		</nav>
+	<?php } 
+} else {
+	if ( $wp_query->max_num_pages > 1 ) {
+		$prev = get_previous_posts_link();
+		$next = get_next_posts_link();
+		if ( $prev || $next ) { ?>
+			<nav id="pagination" class="row" role="navigation">
+				<?php if ( $prev ) {
+					previous_posts_link('&laquo; Newer');
+				}
+				if ( $next ) {
+					next_posts_link('Older &raquo;');
+				} ?>
+			</nav>
+		<?php }
+	}	
+}
