bd6ca61cc2f78aa92dca665f7e619450e6063719
ascottmccauley <scott@ascottmccauley.com>
2015-04-18 11:05:12 -0400

    Center Text

--- 404.php
+++ 404.php
@@ -7,6 +7,6 @@
  */
 ?>
 <?php get_header(); ?>
-<h4><?php _e( 'It seems as though something or someone got lost.' ); ?></h4>
-<h5><?php _e( 'Sorry about the inconvenience.' ); ?></h5>
+<h4 class="text-center"><?php _e( 'It seems as though something or someone got lost.' ); ?></h4>
+<h5 class="text-center"><?php _e( 'Sorry about the inconvenience.' ); ?></h5>
 <?php get_footer(); ?>
