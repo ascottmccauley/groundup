5daa10730a9b328f3731109c50b1b257d02754af
ascottmccauley <scott@ascottmccauley.com>
2015-05-22 08:32:51 -0400

    Remove more WP Styles in head

--- includes/cleanup.php
+++ includes/cleanup.php
@@ -12,7 +12,9 @@ remove_action( 'wp_head', 'wlwmanifest_link' );
 remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
 remove_action( 'wp_head', 'wp_generator' );
 remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
-remove_action( 'wp_head', '_admin_bar_bump_cb' );
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
 
 // Enable compression in .htaccess
 if ( !function_exists( 'groundup_compression' ) ) {
