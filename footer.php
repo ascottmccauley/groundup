5bdf511fb041e07abbd51a4f08a55490d9a5168a
Scott McCauley <scottmccauley@SuzySmall.local>
2015-08-23 11:03:28 -0400

    Update footer to use groundup_get_menu_object()

--- footer.php
+++ footer.php
@@ -10,11 +10,15 @@
 <?php do_action( 'groundup_before_footer' ); ?>
 <footer id="footer">
     <?php do_action( 'groundup_inside_footer' ); ?>
-	<?php if ( has_nav_menu( 'footer' ) ) {
-		wp_nav_menu( array( 'theme_location' => 'footer', 'container' => 'false' ) );
+	<?php $menu_object = groundup_get_menu_object( 'Footer' );
+  if ( $menu_object->count > 0 ) {
+		wp_nav_menu( array(
+      'menu' => $menu_object->term_id,
+      'container' => 'false',
+    ) );
 	} else { ?>
 		<div class="copyrights">
-			<a href="<?php echo esc_url(get_permalink(get_page_by_title( 'Copyrights'))); ?>">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></a>
+			<a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Copyrights' ) ) ); ?>">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></a>
 		</div>
 	<?php } ?>
 </footer>
