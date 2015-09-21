87d63f69624e0f3ba24c27b478e261daa0e792ca
Scott McCauley <scottmccauley@SuzySmall.local>
2015-09-21 14:44:27 -0400

    =Add footer page content

--- footer.php
+++ footer.php
@@ -10,13 +10,23 @@
 <?php do_action( 'groundup_before_footer' ); ?>
 <footer id="footer">
     <?php do_action( 'groundup_inside_footer' ); ?>
-	<?php $menu_object = groundup_get_menu_object( 'Footer' );
+  <?php // look for contents of a 'footer' page
+  $page = get_page_by_title( 'footer' );
+  if ( $page ) {
+      echo '<div class="footer-content">';
+      echo apply_filters( 'the_content', $page->post_content );
+      echo '</div>';
+  }
+  // look for a 'footer' menu
+  $menu_object = groundup_get_menu_object( 'Footer' );
   if ( $menu_object->count > 0 ) {
 		wp_nav_menu( array(
       'menu' => $menu_object->term_id,
       'container' => 'false',
     ) );
-	} else { ?>
+	}
+  // default to generic copyright message
+  if ( !$page && !( $menu_object->count > 0 ) ) { ?>
 		<div class="copyrights">
 			<a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Copyrights' ) ) ); ?>">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></a>
 		</div>
